00d2a99a1422f96aa386b5a57a7417b198cbd692
Alparslan B. Ari <burhanari@192.168.0.11>
2017-11-04 20:08:18 +0100

    Ændring i Dice klassen

--- src/Game/Dice.java
+++ src/Game/Dice.java
@@ -2,31 +2,14 @@ package Game;
 
 public class Dice {
 
-	private int dice;
-	private int sum;	
+	private int faceValue;
+	private int diceside = 6;
 	
 	public void rollDice() {
-		dice = (int)(Math.random()*6) + 1; //Terning 1 sættes til at være 1-6
-		//dice2 = (int)(Math.random()*6) + 1; //Terning 2 sættes til at være 1-6
-		diceSum();
+		faceValue = (int)(Math.random()*diceside) + 1; //Terning 1 sættes til at være 1-6
 	}
 	
-	public void diceSum() {
-		sum = dice + dice; //Finder summen af de to terninger
-	}
-
-	public boolean getEns() {
-		if(dice == dice) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
 	
-	public int getSum() {
-		return sum;
-	}
 	
 }
 
