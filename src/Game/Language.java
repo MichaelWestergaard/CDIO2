f226b47071887d72edad63c65215f2773941da44
Alparslan B. Ari <burhanari@192.168.0.11>
2017-11-06 15:39:39 +0100

    Language læseren

--- src/Game/Language.java
+++ src/Game/Language.java
@@ -1,10 +1,33 @@
 package Game;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Language {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String file ="src/Languages/Dansk.txt";
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String currentLine = reader.readLine();
+			reader.close();
+			for(int i=0;i<40;i++)
+			System.out.println();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
+		
+	}
 
-}
+
