c376ef5a7b3042c7324ed00dff8e3c9169f6eed5
Alparslan B. Ari <burhanari@192.168.0.11>
2017-11-06 16:17:07 +0100

    JUnit testerne for klasserne

---
+++ src/Test/GameTest.java
@@ -0,0 +1,49 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGameSetup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRollDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPlayTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPressEnter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowScore() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/Test/PlayerTest.java
@@ -0,0 +1,34 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+}
