8e7781c56b7709143cd713b10a10051f8ac83152
Mr. Timn <Mr. Timn@MrTimn-PC>
2017-11-09 12:04:45 +0100

    JUnit test af sumToPoints() metoden i Game klassen

--- src/Test/GameTest.java
+++ src/Test/GameTest.java
@@ -2,8 +2,6 @@ package Test;
 
 import static org.junit.Assert.*;
 
-import java.util.Map;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -13,7 +11,8 @@ import Game.Language;
 
 public class GameTest {
 
-	Game game = new Game();
+	Game game;
+	Language lang;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -28,30 +27,24 @@ public class GameTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
+	@Test //Tester om sumToPoints() giver det rigtige antal point for hvert slag.
 	public void testSumToPoints() {
-		Language lang = new Language();
+		game = new Game();
+		lang = new Language();
 		game.gameText = lang.languageSetup("Dansk.txt");
 		
-		//game = new Game();
-		//int complete = 0;
-		//int[] feltvalue = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
-		int sum = 2;
+		int complete = 0;
+		int[] feltvalue = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
 		
+		for(int i=2; i<13; i++) {
+			int expected = feltvalue[i-2];
+			int actual = game.sumToPoints(i);
+				if(expected == actual) {
+					complete++;
+				}
+		}
 		
-		//for(int i=2; i<13; i++) {
-			//int expected = 250;
-			//int actual = game.sumToPoints(sum);
-			//if(expected == actual) {
-				//complete++;
-			//}
-			//System.out.println(complete);
-		//}
-		int points = game.sumToPoints(sum);
-		System.out.println(points);
-		
-		//assertEquals(expected, actual);
-		
+		assertEquals(11, complete);
 	}
 
 }
