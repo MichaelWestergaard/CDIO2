e4aa2b75517bed8247ecb727ca98fdd0fcfe54ff
Mr. Timn <Mr. Timn@MrTimn-PC>
2017-11-09 11:51:57 +0100

    Michael

--- src/Test/GameTest.java
+++ src/Test/GameTest.java
@@ -6,8 +6,12 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import Game.Game;
+
 public class GameTest {
 
+	Game game = new Game();
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -22,8 +26,26 @@ public class GameTest {
 	}
 
 	@Test
-	public void testSumToPoints() {
-		fail("Not yet implemented");
+	public void testSumToPoints() {		
+		//game = new Game();
+		//int complete = 0;
+		//int[] feltvalue = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
+		int sum = 2;
+		
+		
+		//for(int i=2; i<13; i++) {
+			//int expected = 250;
+			//int actual = game.sumToPoints(sum);
+			//if(expected == actual) {
+				//complete++;
+			//}
+			//System.out.println(complete);
+		//}
+		
+		System.out.println(game.sumToPoints(sum));
+		
+		//assertEquals(expected, actual);
+		
 	}
 
 }
